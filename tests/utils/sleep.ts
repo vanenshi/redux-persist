60870774b975b82a200c2f087b4e2bd9659d6eab
Amir Hossein Shekari <vanenshi@gmail.com>
2024-05-22 18:08:24 +0330

    Update

--- tests/utils/sleep.ts
+++
@@ -1,6 +0,0 @@
-export default function (timeout: number): Promise<void> {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  return new Promise((resolve, _) => {
-    setTimeout(resolve, timeout)
-  })
-}
